c886d85f551d612fe289061947891c3e50bdd02d
pdineshyadav <p.dineshyadav@yahoo.in>
2016-11-23 16:00:53 -0500

    Add project files.

---
+++ Comp229Assignment2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Comp229Assignment2.Startup))]
+namespace Comp229Assignment2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
