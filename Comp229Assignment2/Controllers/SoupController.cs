007b28da9ce095690e16ea40202e9ef0cc1167a7
pdineshyadav <p.dineshyadav@yahoo.in>
2016-11-25 01:14:22 -0500

    Update 2

---
+++ Comp229Assignment2/Controllers/SoupController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Comp229Assignment2.Controllers
+{
+    public class SoupController : Controller
+    {
+        // GET: Soup
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
